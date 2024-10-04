162c61a435b75c5ca5e290d15df1def89346f8f2
Kristina Zhuk <sivych26@mail.ru>
2024-10-04 21:13:28 +0300

    add_makefile

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,10 @@
 import os
-
 import pytest
 import sys
 
 
 from selenium import webdriver
+
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
@@ -26,14 +26,14 @@ def pytest_addoption(parser):
 @pytest.fixture
 def driver(request):
     if request.config.getoption("--firefox"):
-        options = FirefoxOptions()
+        options = webdriver.FirefoxOptions()
         gecko_install = GeckoDriverManager().install()
         folder = os.path.dirname(gecko_install)
         geckodriver_path = os.path.join(folder, "geckodriver.exe")
         service = FirefoxService(geckodriver_path)
         driver = webdriver.Firefox(service=service, options=options)
     else:
-        options = ChromeOptions()
+        options = webdriver.ChromeOptions()
         options.add_argument("--disable-search-engine-choice-screen")
         chrome_install = ChromeDriverManager().install()
         folder = os.path.dirname(chrome_install)
@@ -45,3 +45,30 @@ def driver(request):
     yield driver
 
     driver.quit()
+
+
+# import os
+#
+# import pytest
+#
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+#
+#
+# @pytest.fixture
+# def driver():
+#     options = Options()
+# #    options.add_argument('--window-size=1920,1080')
+#     options.add_argument("--disable-search-engine-choice-screen")
+#     chrome_install = ChromeDriverManager().install()
+#     folder = os.path.dirname(chrome_install)
+#     chromedriver_path = os.path.join(folder, "chromedriver.exe")
+#     chrome_service = Service(chromedriver_path)
+#     driver = webdriver.Chrome(service=chrome_service, options=options)
+#     driver.maximize_window()
+#     yield driver
+#
+#     driver.quit()
+#     driver.close()
